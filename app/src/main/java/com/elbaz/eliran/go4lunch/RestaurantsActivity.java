8492541516a5bdf213d38c54d4fb262d88631c17
e210564e <devedelie@gmail.com>
2019-09-19 23:16:06 +0200

    - Added new activity (RestaurantActivity) to launch after login

---
+++ app/src/main/java/com/elbaz/eliran/go4lunch/RestaurantsActivity.java
@@ -0,0 +1,11 @@
+package com.elbaz.eliran.go4lunch;
+
+import com.elbaz.eliran.go4lunch.base.BaseActivity;
+
+public class RestaurantsActivity extends BaseActivity {
+
+    @Override
+    public int getFragmentLayout() {
+        return R.layout.activity_restaurants;
+    }
+}
