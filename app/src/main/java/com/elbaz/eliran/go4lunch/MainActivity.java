c6b973591051c59046aeb68f71a777078b458f66
e210564e <devedelie@gmail.com>
2019-09-19 20:06:12 +0200

    MainActivity: - Added methods to MainActivity to control the action to be taken on-click - Added a method to create a user in Firestore

--- app/src/main/java/com/elbaz/eliran/go4lunch/MainActivity.java
+++ app/src/main/java/com/elbaz/eliran/go4lunch/MainActivity.java
@@ -3,12 +3,19 @@ package com.elbaz.eliran.go4lunch;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import com.elbaz.eliran.go4lunch.api.UserHelper;
 import com.elbaz.eliran.go4lunch.base.BaseActivity;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -24,7 +31,8 @@ public class MainActivity extends BaseActivity {
 
     @BindView(R.id.main_activity_coordinator_layout) CoordinatorLayout coordinatorLayout;
     @BindView(R.id.welcomeText) TextView welcomeText;
-    @BindView(R.id.main_activity_button_login) Button buttonLogin;
+    @BindView(R.id.main_activity_button_gmail) Button gmailButtonLogin;
+    @BindView(R.id.main_activity_button_facebook) Button facebookButtonLogin;
 
     @Override
     public int getFragmentLayout() { return R.layout.activity_main; }
@@ -32,24 +40,60 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        // Avoid login screen if user is already authenticated
+        // Avoid login-screen if the user is already authenticated
         if (this.isCurrentUserLogged()) {
             this.startRestaurantsActivity();
         }
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 4 - Handle SignIn Activity response on activity result
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
 
     // --------------------
     // ACTIONS
     // --------------------
 
-    @OnClick(R.id.main_activity_button_login)
-    public void onClickLoginButton() {
+    @OnClick(R.id.main_activity_button_gmail)
+    public void onClickGmailLoginButton() {
         //Start appropriate activity
-            this.startSignInActivity();
+            this.startSignInActivityWithGmail();
         }
 
+    @OnClick(R.id.main_activity_button_facebook)
+    public void onClickFacebookLoginButton() {
+        //Start appropriate activity
+        this.startSignInActivityWithFacebook();
+    }
+
+    // --------------------
+    // REST REQUEST
+    // --------------------
+
+    //  Http request that create user in firestore
+    private void createUserInFirestore(){
+
+        if (this.getCurrentUser() != null){
+
+            String urlPicture = (this.getCurrentUser().getPhotoUrl() != null) ? this.getCurrentUser().getPhotoUrl().toString() : null;
+            String username = this.getCurrentUser().getDisplayName();
+            String uid = this.getCurrentUser().getUid();
+
+            UserHelper.createUser(uid, username, urlPicture).addOnFailureListener(this.onFailureListener());
+        }
+    }
+
+    // --------------------
+    // UI
+    // --------------------
+
+    // Show Snack Bar with a message
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
 
     // --------------------
     // UTILS
@@ -60,12 +104,35 @@ public class MainActivity extends BaseActivity {
 
     protected Boolean isCurrentUserLogged(){ return (this.getCurrentUser() != null); }
 
+
+    //  Method that handles response after SignIn Activity close
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                // CREATE USER IN FIRESTORE
+                this.createUserInFirestore();
+                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
+    }
+
     // --------------------
     // NAVIGATION
     // --------------------
 
-    // 2 - Launch Sign-In Activity - Firebase UI (not in layouts xmls)
-    private void startSignInActivity(){
+    // Launch Sign-In Activity with Gmail- Firebase UI
+    private void startSignInActivityWithGmail(){
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
@@ -79,10 +146,38 @@ public class MainActivity extends BaseActivity {
                 RC_SIGN_IN);
     }
 
+    // Launch Sign-In Activity with Facebook- Firebase UI
+    private void startSignInActivityWithFacebook(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(
+                                        new AuthUI.IdpConfig.FacebookBuilder().setPermissions(Arrays.asList("default", "email")).build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
     // Launching Restaurants Activity
     private void startRestaurantsActivity(){
         Intent intent = new Intent(this, RestaurantsActivity.class);
         startActivity(intent);
+    }
+
+    // --------------------
+    // ERROR HANDLER
+    // --------------------
+
+    protected OnFailureListener onFailureListener(){
+        return new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), getString(R.string.error_unknown_error), Toast.LENGTH_LONG).show();
+            }
+        };
     }
 
 }
