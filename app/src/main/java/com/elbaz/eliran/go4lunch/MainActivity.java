302641319699ca4b44abc483b316d97fa4232d42
e210564e <devedelie@gmail.com>
2019-09-19 11:44:34 +0200

    - Added basic design elements to project folders (background and logo) - Created a layout for MainActivity - created BaseActivity which MainActivity is now extending from

--- app/src/main/java/com/elbaz/eliran/go4lunch/MainActivity.java
+++ app/src/main/java/com/elbaz/eliran/go4lunch/MainActivity.java
@@ -2,13 +2,17 @@ package com.elbaz.eliran.go4lunch;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.elbaz.eliran.go4lunch.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public int getFragmentLayout() { return R.layout.activity_main; }
+
 }
---
+++ app/src/main/java/com/elbaz/eliran/go4lunch/base/BaseActivity.java
@@ -0,0 +1,38 @@
+package com.elbaz.eliran.go4lunch.base;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Eliran Elbaz on 19-Sep-19.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    // --------------------
+    // LIFE CYCLE
+    // --------------------
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(this.getFragmentLayout());
+        ButterKnife.bind(this); //Configure Butterknife
+    }
+
+    public abstract int getFragmentLayout();
+
+    // --------------------
+    // UI
+    // --------------------
+
+    protected void configureToolbar(){
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+}
