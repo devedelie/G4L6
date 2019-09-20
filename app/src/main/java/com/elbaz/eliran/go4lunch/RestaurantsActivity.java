3de8b76e9d89dcf9de81ea386847778c31f1c86d
e210564e <devedelie@gmail.com>
2019-09-20 17:19:59 +0200

    - Added 3 buttons at the bottom of RestaurantActivity - Added 3 icons - Added googleMap support to manifest - Added Location permissions to manifest - Added MAP fragment to activity_restaurant.xml - Added initialisation of the map on RestaurantActivity.java - Added API_KEY to google_maps_api.xml (debug version)  --> release version is under project tree

--- app/src/main/java/com/elbaz/eliran/go4lunch/RestaurantsActivity.java
+++ app/src/main/java/com/elbaz/eliran/go4lunch/RestaurantsActivity.java
@@ -1,8 +1,15 @@
 package com.elbaz.eliran.go4lunch;
 
 import com.elbaz.eliran.go4lunch.base.BaseActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class RestaurantsActivity extends BaseActivity {
+public class RestaurantsActivity extends BaseActivity implements OnMapReadyCallback{
+    private GoogleMap mMap;
 
     @Override
     public int getFragmentLayout() {
@@ -12,10 +19,31 @@ public class RestaurantsActivity extends BaseActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        // Initialise map
+        this.initMap();
         // Setup the toolbar
         this.configureToolbarWithDrawer();
         // setup the drawer
         this.configureDrawerLayoutAndNavigationView();
     }
 
+    // Initialise Google Map
+    private void initMap(){
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setMinZoomPreference(15.0f);
+        mMap.setMaxZoomPreference(18.0f);
+
+        // Add a marker in Sydney and move the camera
+        LatLng paris = new LatLng(48.864716, 2.349014);
+        mMap.addMarker(new MarkerOptions().position(paris).title("Your Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(paris));
+    }
 }
