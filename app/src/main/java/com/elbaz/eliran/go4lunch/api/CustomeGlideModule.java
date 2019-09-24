35ffc8cf56fdb40fd38e91c4785da4ffeb401c51
e210564e <devedelie@gmail.com>
2019-09-24 17:08:21 +0200

    - Added CustomGlideModule to prevent Error on Logcat

---
+++ app/src/main/java/com/elbaz/eliran/go4lunch/api/CustomeGlideModule.java
@@ -0,0 +1,12 @@
+package com.elbaz.eliran.go4lunch.api;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by Eliran Elbaz on 23-Sep-19.
+ */
+// Solves Error from Logcat
+@GlideModule
+public class CustomeGlideModule extends AppGlideModule {
+}
