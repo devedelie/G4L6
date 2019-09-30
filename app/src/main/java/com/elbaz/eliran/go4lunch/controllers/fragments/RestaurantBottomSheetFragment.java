beb391a9a41efa4f95077fc8ee4d22340a0841bb
e210564e <devedelie@gmail.com>
2019-09-30 16:23:04 +0200

    - Added Result changedListener to BottomSheet (LiveData) - Added Image, title, opening hours, and descriptions to layout

--- app/src/main/java/com/elbaz/eliran/go4lunch/controllers/fragments/RestaurantBottomSheetFragment.java
+++ app/src/main/java/com/elbaz/eliran/go4lunch/controllers/fragments/RestaurantBottomSheetFragment.java
@@ -5,13 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.elbaz.eliran.go4lunch.R;
+import com.elbaz.eliran.go4lunch.models.nearbyPlacesModel.Result;
+import com.elbaz.eliran.go4lunch.viewmodels.SharedViewModel;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,7 +31,11 @@ public class RestaurantBottomSheetFragment extends BottomSheetDialogFragment {
     // FOR DESIGN
     @BindView(R.id.fragment_bottomsheet_recycler_view) RecyclerView recyclerView;
     @BindView(R.id.fragment_detail_image) ImageView fragmentDetailMainImage;
-    private static final String KEY_PROJECT_ID = "KEY_PROJECT_ID";
+    @BindView(R.id.fragment_restaurant_detail_title) TextView restaurantDetailTitle;
+    @BindView(R.id.fragment_restaurant_detail_description) TextView restaurantDetailDescription;
+    @BindView(R.id.detail_restaurant_likes) TextView restaurantDetailLikes;
+    private SharedViewModel mSharedViewModel;
+    private List<Result> mResults;
 
     public static RestaurantBottomSheetFragment newInstance(Integer projectId) {
         RestaurantBottomSheetFragment restaurantBottomSheetFragment;
@@ -44,12 +56,34 @@ public class RestaurantBottomSheetFragment extends BottomSheetDialogFragment {
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        this.setViewElements();
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Set ViewModel Elements under onActivityCreated() to scope it to the lifeCycle of the Fragment
+        mSharedViewModel = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
+        // get fetched Results
+        mSharedViewModel.getResults().observe(getViewLifecycleOwner(), new Observer<List<Result>>() {
+            @Override
+            public void onChanged(List<Result> results) {
+                // set results
+                mResults = new ArrayList<>();
+                mResults.clear();
+                mResults.addAll(results);
+                setViewElements();
+            }
+        });
     }
 
     private void setViewElements(){
         // set image
         Glide.with(this).load(MapViewFragment.bottomSheetMainImageURL).into(this.fragmentDetailMainImage);
+        // set Texts
+        restaurantDetailTitle.setText(mResults.get(0).getName());
+        if(mResults.get(0).getOpeningHours().getOpenNow()){
+            restaurantDetailDescription.setText(getString(R.string.restaurant_detail_openNow));
+        } else{
+            restaurantDetailDescription.setText(getString(R.string.restaurant_detail_closed));
+        }
+        // set rating
+        restaurantDetailLikes.setText(mResults.get(0).getRating().toString());
     }
 }
