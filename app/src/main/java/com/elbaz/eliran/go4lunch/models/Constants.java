d446b796b8e7e40c342bc303a67d8c344572d3bd
e210564e <devedelie@gmail.com>
2019-10-02 12:22:24 +0200

    - Modified the nearby radius to 800m

--- app/src/main/java/com/elbaz/eliran/go4lunch/models/Constants.java
+++ app/src/main/java/com/elbaz/eliran/go4lunch/models/Constants.java
@@ -10,10 +10,9 @@ public class Constants {
     public static final Integer LIST_VIEW_FRAGMENT = 1;
     public static final Integer WORKMATES_FRAGMENT = 2;
     public static final String GOOGLE_MAPS_API_BASE_URL = "https://maps.googleapis.com/";
+    public static final String URL_FOR_IMAGE = "maps/api/place/photo?maxwidth=400&photoreference=";
+    public static final String URL_FOR_IMAGE_KEY = "&key=";
     public static final String NEARBY_TYPE="restaurant";
-    public static final int NEARBY_RADIUS= 1500;
-
-
-
+    public static final int NEARBY_RADIUS= 800;
 
 }
