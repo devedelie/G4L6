4253fb2eb77f1d99fe46616e20ba76a65b1418d7
e210564e <devedelie@gmail.com>
2019-10-03 14:03:52 +0200

    - Added imageView to item - Added all view elements into try/catch condition

--- app/src/main/java/com/elbaz/eliran/go4lunch/views/RestaurantListViewHolder.java
+++ app/src/main/java/com/elbaz/eliran/go4lunch/views/RestaurantListViewHolder.java
@@ -9,12 +9,18 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.elbaz.eliran.go4lunch.BuildConfig;
 import com.elbaz.eliran.go4lunch.R;
 import com.elbaz.eliran.go4lunch.models.nearbyPlacesModel.Result;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
 import static android.content.ContentValues.TAG;
+import static com.elbaz.eliran.go4lunch.models.Constants.GOOGLE_MAPS_API_BASE_URL;
+import static com.elbaz.eliran.go4lunch.models.Constants.URL_FOR_IMAGE;
+import static com.elbaz.eliran.go4lunch.models.Constants.URL_FOR_IMAGE_KEY;
 
 /**
  * Created by Eliran Elbaz on 02-Oct-19.
@@ -27,6 +33,7 @@ public class RestaurantListViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.restaurantList_recyclerView_person) ImageView personImage;
 //    @BindView(R.id.restaurantList_recyclerView_person_number) TextView personNumberTextView;
     @BindView(R.id.restaurantList_recyclerView_stars) ImageView starsImage;
+    @BindView(R.id.restaurantList_recyclerView_restaurantImage) ImageView restaurantImage;
 
     public RestaurantListViewHolder(@NonNull View itemView) {
         super(itemView);
@@ -34,11 +41,21 @@ public class RestaurantListViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void updateRestaurantsList (Result result, RequestManager glide){
-        restaurantNameTextView.setText(result.getName());
-        addressTextView.setText(result.getVicinity());
-        openingTextView.setText(result.getOpeningHours().getOpenNow().toString());
-        distanceTextView.setText(result.getId());
-        Log.d(TAG, "ListView updateRestaurantsList: ");
+        try{
+            //set images
+            String imageUrl = GOOGLE_MAPS_API_BASE_URL + URL_FOR_IMAGE + result.getPhotos().get(0).getPhotoReference() + URL_FOR_IMAGE_KEY + BuildConfig.GOOGLE_BROWSER_API_KEY;
+            Log.d(TAG, "setViewElementsForNearbyPlaces: " + imageUrl);
+            glide.load(imageUrl).apply(RequestOptions.centerCropTransform()).into(restaurantImage);
+            // set texts
+            restaurantNameTextView.setText(result.getName());
+            addressTextView.setText(result.getVicinity());
+            openingTextView.setText(result.getOpeningHours().getOpenNow().toString());
+            distanceTextView.setText(result.getRating().toString());
+            Log.d(TAG, "ListView updateRestaurantsList: ");
+        }catch (Exception e){
+            Log.d(TAG, "updateRestaurantsList: Error " + e);
+        }
+
 
     }
 
