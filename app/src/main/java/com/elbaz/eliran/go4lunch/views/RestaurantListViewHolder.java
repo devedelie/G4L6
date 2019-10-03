2543b66d8af28cbb0a86930c046c2582628776d7
e210564e <devedelie@gmail.com>
2019-10-03 10:43:27 +0200

    - Added view elements with ButterKnife - Added method to update all view elements in recyclerView with the correct data

--- app/src/main/java/com/elbaz/eliran/go4lunch/views/RestaurantListViewHolder.java
+++ app/src/main/java/com/elbaz/eliran/go4lunch/views/RestaurantListViewHolder.java
@@ -1,19 +1,32 @@
 package com.elbaz.eliran.go4lunch.views;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.RequestManager;
+import com.elbaz.eliran.go4lunch.R;
 import com.elbaz.eliran.go4lunch.models.nearbyPlacesModel.Result;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Eliran Elbaz on 02-Oct-19.
  */
 public class RestaurantListViewHolder extends RecyclerView.ViewHolder {
+    @BindView(R.id.restaurantList_recyclerView_restaurantName) TextView restaurantNameTextView;
+    @BindView(R.id.restaurantList_recyclerView_address) TextView addressTextView;
+    @BindView(R.id.restaurantList_recyclerView_openingTimes) TextView openingTextView;
+    @BindView(R.id.restaurantList_recyclerView_distance) TextView distanceTextView;
+    @BindView(R.id.restaurantList_recyclerView_person) ImageView personImage;
+//    @BindView(R.id.restaurantList_recyclerView_person_number) TextView personNumberTextView;
+    @BindView(R.id.restaurantList_recyclerView_stars) ImageView starsImage;
 
     public RestaurantListViewHolder(@NonNull View itemView) {
         super(itemView);
@@ -21,7 +34,12 @@ public class RestaurantListViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void updateRestaurantsList (Result result, RequestManager glide){
-        
+        restaurantNameTextView.setText(result.getName());
+        addressTextView.setText(result.getVicinity());
+        openingTextView.setText(result.getOpeningHours().getOpenNow().toString());
+        distanceTextView.setText(result.getId());
+        Log.d(TAG, "ListView updateRestaurantsList: ");
+
     }
 
 
