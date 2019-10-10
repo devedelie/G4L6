41085ddb8f5418e356b5ecdc5e194fbf410a5446
e210564e <devedelie@gmail.com>
2019-10-10 13:32:50 +0200

    Added stream call with RxJava & Retrofit

--- app/src/main/java/com/elbaz/eliran/go4lunch/utils/GoogleMapApiService.java
+++ app/src/main/java/com/elbaz/eliran/go4lunch/utils/GoogleMapApiService.java
@@ -1,6 +1,7 @@
 package com.elbaz.eliran.go4lunch.utils;
 
 import com.elbaz.eliran.go4lunch.models.nearbyPlacesModel.PlacesResults;
+import com.elbaz.eliran.go4lunch.models.restaurantDetails.RestaurantDetails;
 
 import io.reactivex.Observable;
 import retrofit2.Retrofit;
@@ -15,19 +16,26 @@ import static com.elbaz.eliran.go4lunch.models.Constants.GOOGLE_MAPS_API_BASE_UR
  * Created by Eliran Elbaz on 26-Sep-19.
  */
 // URL to fetch nearby restaurants within 1500m around the location (return JSON)
-// https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&key=API_KEY
+//Request example: https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&key=API_KEY
 
 public interface GoogleMapApiService {
     @GET("maps/api/place/nearbysearch/json")
-    Observable<PlacesResults> getRestaurants (@Query("location") String location,
-                                              @Query("radius") int radius,
-                                              @Query("type") String type,
-                                              @Query("key") String key);
+    Observable<PlacesResults> getNearbyRestaurants(@Query("location") String location,
+                                                   @Query("radius") int radius,
+                                                   @Query("type") String type,
+                                                   @Query("key") String key);
 
-// https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=CpQCAgEAAFxg8o-eU7_uKn7Yqjana-HQIx1hr5BrT4zBaEko29ANsXtp9mrqN0yrKWhf-y2PUpHRLQb1GT-mtxNcXou8TwkXhi1Jbk-ReY7oulyuvKSQrw1lgJElggGlo0d6indiH1U-tDwquw4tU_UXoQ_sj8OBo8XBUuWjuuFShqmLMP-0W59Vr6CaXdLrF8M3wFR4dUUhSf5UC4QCLaOMVP92lyh0OdtF_m_9Dt7lz-Wniod9zDrHeDsz_by570K3jL1VuDKTl_U1cJ0mzz_zDHGfOUf7VU1kVIs1WnM9SGvnm8YZURLTtMLMWx8-doGUE56Af_VfKjGDYW361OOIj9GmkyCFtaoCmTMIr5kgyeUSnB-IEhDlzujVrV6O9Mt7N4DagR6RGhT3g1viYLS4kO5YindU6dm3GIof1Q&key=YOUR_API_KEY
+//Request example: https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=CpQCAgEAAFxg8o-eU7_uKn7Yqjana-HQIx1hr5BrT4zBaEko29ANsXtp9mrqN0yrKWhf-y2PUpHRLQb1GT-mtxNcXou8TwkXhi1Jbk-ReY7oulyuvKSQrw1lgJElggGlo0d6indiH1U-tDwquw4tU_UXoQ_sj8OBo8XBUuWjuuFShqmLMP-0W59Vr6CaXdLrF8M3wFR4dUUhSf5UC4QCLaOMVP92lyh0OdtF_m_9Dt7lz-Wniod9zDrHeDsz_by570K3jL1VuDKTl_U1cJ0mzz_zDHGfOUf7VU1kVIs1WnM9SGvnm8YZURLTtMLMWx8-doGUE56Af_VfKjGDYW361OOIj9GmkyCFtaoCmTMIr5kgyeUSnB-IEhDlzujVrV6O9Mt7N4DagR6RGhT3g1viYLS4kO5YindU6dm3GIof1Q&key=YOUR_API_KEY
     @GET("maps/api/place/nearbysearch/json")
     Observable<PlacesResults> getNextPageToken (@Query("pagetoken") String pageToken,
                                                 @Query("key") String key);
+
+
+// Request example: https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating,formatted_phone_number&key=YOUR_API_KEY
+    @GET("maps/api/place/details/json")
+    Observable<RestaurantDetails> getRestaurantDetailsByID (@Query("place_id") String location,
+                                                            @Query("fields") String radius,
+                                                            @Query("key") String key);
 
 
     Retrofit retrofit = new Retrofit.Builder()
--- app/src/main/java/com/elbaz/eliran/go4lunch/utils/PlacesStream.java
+++ app/src/main/java/com/elbaz/eliran/go4lunch/utils/PlacesStream.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.elbaz.eliran.go4lunch.BuildConfig;
 import com.elbaz.eliran.go4lunch.models.nearbyPlacesModel.PlacesResults;
+import com.elbaz.eliran.go4lunch.models.restaurantDetails.RestaurantDetails;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,6 +12,7 @@ import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 import static android.content.ContentValues.TAG;
+import static com.elbaz.eliran.go4lunch.models.Constants.SEARCH_FIELDS;
 
 /**
  * Created by Eliran Elbaz on 28-Sep-19.
@@ -20,15 +22,24 @@ public class PlacesStream {
     public static Observable<PlacesResults> streamFetchNearbyLocations(String location, int radius, String type){
         Log.d(TAG, "streamFetchNearbyLocations: " + location + " " + radius + " " + type + " " + BuildConfig.GOOGLE_BROWSER_API_KEY);
         GoogleMapApiService googleMapApiService = GoogleMapApiService.retrofit.create(GoogleMapApiService.class);
-        return googleMapApiService.getRestaurants(location, radius, type, BuildConfig.GOOGLE_BROWSER_API_KEY)
+        return googleMapApiService.getNearbyRestaurants(location, radius, type, BuildConfig.GOOGLE_BROWSER_API_KEY)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .timeout(10, TimeUnit.SECONDS);
     }
 
-    public static Observable<PlacesResults> streamFetchNextPageToken(String token){
+//    public static Observable<PlacesResults> streamFetchNextPageToken(String token){
+//        GoogleMapApiService googleMapApiService = GoogleMapApiService.retrofit.create(GoogleMapApiService.class);
+//        return googleMapApiService.getNextPageToken(token, BuildConfig.GOOGLE_BROWSER_API_KEY)
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .timeout(10, TimeUnit.SECONDS);
+//    }
+
+    public static Observable<RestaurantDetails> streamFetchRestaurantDetailsByID(String restaurantID){
         GoogleMapApiService googleMapApiService = GoogleMapApiService.retrofit.create(GoogleMapApiService.class);
-        return googleMapApiService.getNextPageToken(token, BuildConfig.GOOGLE_BROWSER_API_KEY)
+        Log.d(TAG, "streamFetchRestaurantDetailsByID: "+restaurantID +" "+ SEARCH_FIELDS+ " "+ BuildConfig.GOOGLE_BROWSER_API_KEY);
+        return googleMapApiService.getRestaurantDetailsByID(restaurantID, SEARCH_FIELDS, BuildConfig.GOOGLE_BROWSER_API_KEY)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .timeout(10, TimeUnit.SECONDS);
