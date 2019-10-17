1531ee641f6a1ba4481464bd6c8d7cb4b68b122f
e210564e <devedelie@gmail.com>
2019-10-17 23:06:14 +0200

    - Javadoc & code cleaning

--- app/src/main/java/com/elbaz/eliran/go4lunch/models/Constants.java
+++ app/src/main/java/com/elbaz/eliran/go4lunch/models/Constants.java
@@ -6,9 +6,6 @@ package com.elbaz.eliran.go4lunch.models;
 public class Constants {
     public static final Integer ERROR_DIALOG_REQUEST = 9001;
     public static final Integer PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
-    public static final Integer MAP_VIEW_FRAGMENT = 0;
-    public static final Integer LIST_VIEW_FRAGMENT = 1;
-    public static final Integer WORKMATES_FRAGMENT = 2;
     public static final String GOOGLE_MAPS_API_BASE_URL = "https://maps.googleapis.com/";
     public static final String URL_FOR_IMAGE = "maps/api/place/photo?maxwidth=400&photoreference=";
     public static final String URL_FOR_IMAGE_KEY = "&key=";
