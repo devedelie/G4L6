229b16f9fb8767f4349ce89fadde5df8ac4973a4
e210564e <devedelie@gmail.com>
2019-10-24 13:23:05 +0200

    - Added a new stream test for location by its ID

--- app/src/androidTest/java/com/elbaz/eliran/go4lunch/NearByPlacesStreamTest.java
+++ app/src/androidTest/java/com/elbaz/eliran/go4lunch/NearByPlacesStreamTest.java
@@ -6,8 +6,8 @@ import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.elbaz.eliran.go4lunch.models.nearbyPlacesModel.PlacesResults;
 import com.elbaz.eliran.go4lunch.models.nearbyPlacesModel.Result;
+import com.elbaz.eliran.go4lunch.models.restaurantDetails.RestaurantDetails;
 import com.elbaz.eliran.go4lunch.utils.PlacesStream;
-import com.google.android.gms.location.FusedLocationProviderClient;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -23,14 +23,13 @@ import io.reactivex.observers.TestObserver;
 import static com.elbaz.eliran.go4lunch.models.Constants.NEARBY_RADIUS;
 import static com.elbaz.eliran.go4lunch.models.Constants.NEARBY_TYPE;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
-import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertEquals;
 
 
 @RunWith(JUnit4.class)
 public class NearByPlacesStreamTest {
-    private FusedLocationProviderClient mFusedLocationProviderClient;
-    private String deviceLocationTest= "48.831212,2.329754";
+    private final String deviceLocationTest= "48.831212,2.329754";
+    private final String placeId = "ChIJvz3Ntrlx5kcRCylrI3QARlw";
 
     @Test
     public void useAppContext() {
@@ -39,12 +38,15 @@ public class NearByPlacesStreamTest {
         assertEquals("com.elbaz.eliran.go4lunch", appContext.getPackageName());
     }
 
+    //----------------------
+    // Nearby-places stream
+    //----------------------
+
     @Test
-    public void isStream_fetchNearbyPlacesStream_returnData() {
+    public void isStream_fetchNearbyPlacesStream_returnCorrectData() {
         List<String> placeType = new ArrayList<>(); // List for place type[]
 
         Observable<PlacesResults> observableNearbyPlaces = PlacesStream.streamFetchNearbyLocations(deviceLocationTest, NEARBY_RADIUS, NEARBY_TYPE);
-
         TestObserver<PlacesResults> nearbyPlacesTestObserver = new TestObserver<>();
 
         observableNearbyPlaces.subscribeWith(nearbyPlacesTestObserver)
@@ -64,27 +66,27 @@ public class NearByPlacesStreamTest {
         }
     }
 
+    //--------------------------
+    // Place details stream
+    //--------------------------
 
     @Test
-    public void isStream_fetchNearbyPlacesStreamForBanks_verifyResultsNotContainingRestaurant() {
-        List<String> placeType = new ArrayList<>(); // List for place type[]
+    public void isStream_fetchRestaurantDetailsByID_returnCorrectRestaurantData() {
+        Observable<RestaurantDetails> observableRestaurantDetails = PlacesStream.streamFetchRestaurantDetailsByID(placeId);
+        TestObserver<RestaurantDetails> restaurantDetailsTestObserver = new TestObserver<>();
 
-        Observable<PlacesResults> observableNearbyPlaces = PlacesStream.streamFetchNearbyLocations(deviceLocationTest, NEARBY_RADIUS, "bank");
-
-        TestObserver<PlacesResults> nearbyPlacesTestObserver = new TestObserver<>();
-
-        observableNearbyPlaces.subscribeWith(nearbyPlacesTestObserver)
+        observableRestaurantDetails.subscribeWith(restaurantDetailsTestObserver)
                 .assertNoErrors()
                 .assertNoTimeout()
                 .awaitTerminalEvent();
 
-        PlacesResults placesResults = nearbyPlacesTestObserver.values().get(0);
-        Assert.assertTrue(placesResults.getResults().size() > 0);
-        for(Result result: placesResults.getResults()){
-            // Add specific place types into a list and verify that it HAS NOT "restaurant" type in it
-            placeType.addAll(result.getTypes());
-            Assert.assertThat(placeType, not(hasItem(NEARBY_TYPE)));
-        }
+        RestaurantDetails restaurantDetails = restaurantDetailsTestObserver.values().get(0);
+        // Verify that critical data is not null/empty
+        Assert.assertTrue(restaurantDetails.getResult().getPlaceId() != null && !restaurantDetails.getResult().getPlaceId().isEmpty());
+        Assert.assertTrue(restaurantDetails.getResult().getName() != null && !restaurantDetails.getResult().getName().isEmpty());
+        // Verify that the correct data is returned to the user
+        Assert.assertEquals("ChIJvz3Ntrlx5kcRCylrI3QARlw", restaurantDetails.getResult().getPlaceId());
+        Assert.assertEquals("Café Oz Denfert", restaurantDetails.getResult().getName());
     }
 
 }
